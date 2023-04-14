64f6bdd686ae4cb84ee318689469a513fb014369
jerry <82009379@corphq.hk.pccw.com>
2023-04-14 18:26:09 +0800

    start: init project

---
+++ .history/vite.config_20230414181329.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+const path = require('path');
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // 配置路径别名
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: '',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  css: {
+    postcss: {},
+  },
+});
